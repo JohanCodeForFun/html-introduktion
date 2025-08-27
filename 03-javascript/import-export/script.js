feb7836bae3861ec104ffe0e4db045961093c460
JohanCodeForFun <hellberg.johan@hotmail.com>
2025-08-27 11:55:59 +0200

    clean up script.js for unused imports

--- 03-javascript/import-export/script.js
+++ 03-javascript/import-export/script.js
@@ -1,13 +1,4 @@
-/* =========================================
-   app.js – omskriven till funktionsstil
-   Allt i en fil, redo att delas upp i moduler.
-========================================= */
-
-import { add, average } from "./utils/mathUtils.js";
-import { capitalize, slugify, formatPercent } from "./utils/utils.js";
-import { save, load, clearStorage } from "./services/services.js";
-import { renderTodos, renderStateDump } from "./ui-helpers/ui-helpers.js";
-import { qs, uid } from "./ui-helpers/helpers.js";
+import { uid } from "./ui-helpers/helpers.js";
 import { initUI } from "./ui-helpers/init.js";
 
 // ---------- State ----------
